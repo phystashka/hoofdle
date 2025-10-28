7bc699af2ee03e81d827047ca52eb042b68aca97
shaurmyauu <vappareon@gmail.com>
2025-10-28 13:48:27 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hoofdle/',
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
-    }
-  },
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        format: 'es'
-      }
-    }
-  },
-  define: {
-    'process.env': {}
-  }
+  base: '/hoofdle/'
 })
