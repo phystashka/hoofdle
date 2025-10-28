c1d2cbd560de68ccdbd913828f3aa1d55fa56a92
shaurmyauu <vappareon@gmail.com>
2025-10-28 13:24:50 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/hoofdle/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
